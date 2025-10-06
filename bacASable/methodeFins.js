fb347edb2fcb1731da923d04960debbce8192b18
trobert <timothee.robert@gmail.com>
2025-10-06 10:30:37 +0200

    Modifs mineures

---
+++ bacASable/methodeFins.js
@@ -0,0 +1,10 @@
+const notesRobert = [12,16,18 ]
+const nomRobert = "Robert"
+
+// const okRobert = nomRobert.find(nom => nom === 'Rpbert')
+
+// console.log(okRobert)
+
+const okNotes = notesRobert.find(uneNote => uneNote > 12)
+
+console.log(okNotes)
