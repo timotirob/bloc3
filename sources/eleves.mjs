81ee320231a2f5d0a0834562e556a6b3d013adea
trobert <timothee.robert@gmail.com>
2025-10-06 09:05:59 +0200

    Correction exemple

---
+++ sources/eleves.mjs
@@ -0,0 +1,7 @@
+const listeEleves = [
+    {id: 1, nom: "Robert", prenom: "Timo", age:54},
+    {id: 2, nom: "Belmokhtar", prenom: "Mohamed", age:19},
+    {id:3, nom: "Djalo", prenom: "Amadou", age:19}
+]
+
+export {listeEleves}
